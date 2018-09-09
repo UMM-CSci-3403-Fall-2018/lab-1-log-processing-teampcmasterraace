382eabbd321e0054d5aa66356d93ef25bb0ee191
SirLich <lrkoehler77@gmail.com>
2018-09-08 20:52:20 -0500

    Attempted to add support for native login attempts
    Currently broken

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -5,7 +5,7 @@ dir=$1
 out="failed_login_data.txt"
 cd $dir
 re="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* user ([a-z]+) from (.*) port"
-
+rere="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* for ([a-z]+) from (.*) port"
 #Create the output folder
 touch $out
 
@@ -15,15 +15,14 @@ for file in *; do
         echo "$file"
 	cat $file | while read line
 	do
-		if [[ $line =~ $re ]]
+           if [[ $line =~ $re || $rere ]]
 		then
 			month="${BASH_REMATCH[1]}"
 			day="${BASH_REMATCH[2]}"
 			hour="${BASH_REMATCH[3]}"
 			user="${BASH_REMATCH[4]}"
 			ip="${BASH_REMATCH[5]}"
-			echo "$month $day $hour $user $ip"
-			echo "MATCH"
+			echo "$month $day $hour $user $ip" >> $out
 		fi	
 	done
     fi

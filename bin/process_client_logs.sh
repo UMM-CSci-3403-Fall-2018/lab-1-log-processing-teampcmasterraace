633c07a19c93b577707cea2f3cd30a15a873c0f0
SirLich <lrkoehler77@gmail.com>
2018-09-08 20:34:42 -0500

    Added regex functionality
    Now correctly creates output folder, and adds lines as matched by regex

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
-Directory=$1
+
+#Setup 
+dir=$1
+out="failed_login_data.txt"
+cd $dir
+re="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* user ([a-z]+) from (.*) port"
+
+#Create the output folder
+touch $out
+
+#Iterate over all files and lines
+for file in *; do
+    if [ -f "$file" ] && [ $file != $out ]; then
+        echo "$file"
+	cat $file | while read line
+	do
+		if [[ $line =~ $re ]]
+		then
+			month="${BASH_REMATCH[1]}"
+			day="${BASH_REMATCH[2]}"
+			hour="${BASH_REMATCH[3]}"
+			user="${BASH_REMATCH[4]}"
+			ip="${BASH_REMATCH[5]}"
+			echo "$month $day $hour $user $ip"
+			echo "MATCH"
+		fi	
+	done
+    fi
+done
+
+

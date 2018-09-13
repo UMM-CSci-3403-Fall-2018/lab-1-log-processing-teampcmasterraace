42051dfab5cd05e65ec1d4c427b3ac2af98cec29
SirLich <lrkoehler77@gmail.com>
2018-09-12 19:32:48 -0500

    Temp files are now created in the suplied directory, and are cleaned up after use.
    This is a quality-of-life update which insures the script can be called multiple times without generating extraneous file

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 dir=$1
-touch temp_output
-touch out
+touch $dir/temp_output
+touch $dir/out
 touch $dir/username_dist.html
 
 for file in $dir/*/failed_login_data.txt
 do
-	echo
-	echo "Handling: $file"
-	cat $file >> temp_output
+	cat $file >> $dir/temp_output
 done
 
-awk '{print $4}' $file | sort | uniq -c | awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' >> out
-./bin/wrap_contents.sh out html_components/username_dist $dir/username_dist.html
+awk '{print $4}' $file | sort | uniq -c | awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' >> $dir/out
+./bin/wrap_contents.sh $dir/out html_components/username_dist $dir/username_dist.html
+rm $dir/out
+rm $dir/temp_output
 
 

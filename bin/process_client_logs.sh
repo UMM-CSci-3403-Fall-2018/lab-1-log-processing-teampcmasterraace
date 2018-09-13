97fb88dced1baf6d38135d7dfda6d1156c01b6c2
Prince Nwaonicha <nwaon005@avenger.morris.umn.edu>
2018-09-13 10:12:09 -0500

    This is the updated process client logs. Using awk still in progress.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Setup 
+#Setup
 dir=$1
 out="failed_login_data.txt"
 cd $dir
@@ -11,24 +11,22 @@ touch $out
 
 #Iterate over all files and lines
 for file in *; do
-	{$awk '{
+        {$awk '{
            if (system(grep -q invalid "$file"))
-		then
-			month = $1
-			day = $2
-			hour = substr($3, 0, 2)
-			user = $11
-			ip = $13
-			echo "$month $day $hour $user $ip" >> $out
-	   else
-	       month = $1
-	       day = $2
-	       hour = substr($3, 0, 2)
-	       user = $9
-	       ip = $11
-	       echo "$month $day $hour $user $ip" >> $out
-	       }'
+                then
+                        month = $1
+                        day = $2
+                        hour = substr($3, 0, 2)
+                        user = $11
+                        ip = $13
+                        echo "$month $day $hour $user $ip" >> $out
+           else
+               month = $1
+               day = $2
+               hour = substr($3, 0, 2)
+               user = $9
+               ip = $11
+               echo "$month $day $hour $user $ip" >> $out
+               }'
        }
 done
-
-

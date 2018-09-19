d92aaf01df72b9949b1e880596fa7b83822ad3eb
SirLich <lrkoehler77@gmail.com>
2018-09-18 23:21:37 -0500

    Initial commit. Base file is create_username_dist.sh

---
+++ bin/create_country_dist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Setup
+dir=$1
+touch $dir/temp_output
+touch $dir/out
+touch $dir/username_dist.html
+
+for file in $dir/*/failed_login_data.txt
+do
+	cat $file >> $dir/temp_output
+done
+
+awk '{print $4}' $dir/temp_output | sort | uniq -c | awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' >> $dir/out
+./bin/wrap_contents.sh $dir/out html_components/username_dist $dir/username_dist.html
+rm $dir/out
+rm $dir/temp_output

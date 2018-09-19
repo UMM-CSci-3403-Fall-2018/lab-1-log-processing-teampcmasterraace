a0568f411ed2a3344265b6962b631df94b79d9cf
SirLich <lrkoehler77@gmail.com>
2018-09-18 23:38:58 -0500

    Refactored temp_output and out to be more readable.
    Also added comments for readability.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,18 +2,31 @@
 
 #Setup
 dir=$1
-touch $dir/temp_output
-touch $dir/out
+
+#Temp output. Used for concatinating log files together for proccessing.
+touch $dir/temp_out
+
+#The complete set of generated data, pre-wrapping.
+touch $dir/data_out
+
+#True final output location of the data generated in this script.
 touch $dir/username_dist.html
 
+#Loop to wrap all un-proccesed data into a single file: temp_out
 for file in $dir/*/failed_login_data.txt
 do
-	cat $file >> $dir/temp_output
+	cat $file >> $dir/temp_out
 done
 
-awk '{print $4}' $dir/temp_output | sort | uniq -c | awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' >> $dir/out
-./bin/wrap_contents.sh $dir/out html_components/username_dist $dir/username_dist.html
+#Process temp_out, and send results to data_out
+awk '{print $4}' $dir/temp_out | #Pick the data we want. $4 in this context refers to the username data.
+sort | #Sort, in preperation for the next process command.
+uniq -c | #Count the now sorted lines. Returns a touple with $1 the number of counted lines, and $2 the (username) data counted.
+awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' >> $dir/data_out #Process the data into the required format, and send to data_out
+
+#Wraps the contents of data_out in the correct HEADER and FOOTER files.
+./bin/wrap_contents.sh $dir/data_out html_components/username_dist $dir/username_dist.html
+
+#Clean up temp-files
 rm $dir/out
-rm $dir/temp_output
-
-
+rm $dir/temp_out

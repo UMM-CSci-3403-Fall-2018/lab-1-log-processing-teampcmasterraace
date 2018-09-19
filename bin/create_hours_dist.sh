370f61aacfcb95e9cd7b183868f8c88287df0e9a
SirLich <lrkoehler77@gmail.com>
2018-09-18 23:19:29 -0500

    Edited to match the requirments.
    All tests pass. awks Split was usefull for teasing the hours out.

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -4,14 +4,14 @@
 dir=$1
 touch $dir/temp_output
 touch $dir/out
-touch $dir/username_dist.html
+touch $dir/hours_dist.html
 
 for file in $dir/*/failed_login_data.txt
 do
 	cat $file >> $dir/temp_output
 done
 
-awk '{print $4}' $dir/temp_output | sort | uniq -c | awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' >> $dir/out
-./bin/wrap_contents.sh $dir/out html_components/username_dist $dir/username_dist.html
+awk '{print $3}' $dir/temp_output | awk '{split($0,clock,":")} {print clock[1]}'| sort | uniq -c | awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' >> $dir/out
+./bin/wrap_contents.sh $dir/out html_components/hours_dist $dir/hours_dist.html
 rm $dir/out
 rm $dir/temp_output

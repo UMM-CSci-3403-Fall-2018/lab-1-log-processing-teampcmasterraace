66ac4a38b04d4c50df12342c112de096928b9b04
SirLich <lrkoehler77@gmail.com>
2018-09-19 20:07:55 -0500

    First pass at writing the logic for this file
    Test don't pass, but the logic is 90% of the way there

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+#Author SirLich (Liam Koehle)
+#GitHub: https://github.com/SirLich
+
 #Setup
 dir=$1
-touch $dir/temp_output
-touch $dir/out
-touch $dir/username_dist.html
+touch $dir/temp_out
+touch $dir/data_out
+touch $dir/country_dist.html
 
 for file in $dir/*/failed_login_data.txt
 do
-	cat $file >> $dir/temp_output
+	cat $file >> $dir/temp_out
 done
 
-awk '{print $4}' $dir/temp_output | sort | uniq -c | awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' >> $dir/out
-./bin/wrap_contents.sh $dir/out html_components/username_dist $dir/username_dist.html
-rm $dir/out
-rm $dir/temp_output
+awk '{ print $5}' $dir/temp_out |
+	sort |
+	join -1 1 -2 1 etc/country_IP_map.txt - |
+	awk ' { print $2}' |
+	uniq -c |
+	awk '{printf("data.addRow([\x27%s\x27, %s]);\n",$2 ,$1)}' |
+	sort >> $dir/data_out
+
+  ./bin/wrap_contents.sh $dir/data_out html_components/country_dist $dir/country_dist.html
+
+  rm $dir/data_out
+  rm $dir/temp_out

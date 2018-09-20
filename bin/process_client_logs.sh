edeb8cb0a871a07e5cfe5b12916f8fa1b7db7528
Prince Nwaonicha <nwaon005@avenger.morris.umn.edu>
2018-09-19 20:21:49 -0500

    Prince had issues with git getting unsynced. This is our fix

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 
-#Setup
-dir=$1
-out="failed_login_data.txt"
+#setup
+file= $2
+dir= $1
+out= 'failed_login_data.txt'
 cd $dir
-re="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* user ([a-z]+) from (.*) port"
-rere="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* for ([a-z]+) from (.*) port"
+
 #Create the output folder
 touch $out
 
 #Iterate over all files and lines
-for file in *; do
-        {$awk '{
-           if (system(grep -q invalid "$file"))
-                then
-                        month = $1
-                        day = $2
-                        hour = substr($3, 0, 2)
-                        user = $11
-                        ip = $13
-                        echo "$month $day $hour $user $ip" >> $out
-           else
-               month = $1
-               day = $2
-               hour = substr($3, 0, 2)
-               user = $9
-               ip = $11
-               echo "$month $day $hour $user $ip" >> $out
-               }'
-       }
+for file in *;
+do
+	grep -q 'Failed password for' $file | awk '/invalid user/ echo {"$1 $2 substr($3,0,2) $11 $13"} /Failed password for/ echo {"$1 $2 substr($3,0,2) $9 $11"}' > $out
 done
+

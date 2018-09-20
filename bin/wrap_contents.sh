5d2951382ef53bbc3dba9621848173f63224588b
SirLich <lrkoehler77@gmail.com>
2018-09-20 11:44:56 -0500

    Removed an uneeded echo command + comments & author credit

--- bin/wrap_contents.sh
+++ bin/wrap_contents.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo
+#Author: SirLich (Liam Koehler)
 
+#Setup
 content=$1
 wrapper=$2
 target=$3
-
 footer=${wrapper}_footer.html
 header=${wrapper}_header.html
 
+#Create
 cat $header $content $footer > $target

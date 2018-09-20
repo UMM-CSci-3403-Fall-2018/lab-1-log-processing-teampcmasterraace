0211f2a15cd05e63230d8ac8fcf55ce0d247de36
Prince Nwaonicha <nwaon005@avenger.morris.umn.edu>
2018-09-19 21:33:12 -0500

    Reverted to original regex solution

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-#setup
-file= $2
-dir= $1
-out= 'failed_login_data.txt'
+#Setup 
+dir=$1
+out="failed_login_data.txt"
 cd $dir
+re="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* user ([a-z]+) from (.*) port"
+
+re2="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* for ([a-z]+) from (.*) port"
 
 #Create the output folder
 touch $out
-
 #Iterate over all files and lines
-for file in *;
-do
-	grep -q 'Failed password for' $file | awk '/invalid user/ echo {"$1 $2 substr($3,0,2) $11 $13"} /Failed password for/ echo {"$1 $2 substr($3,0,2) $9 $11"}' > $out
-done
+for file in *; do
+    if [ -f "$file" ] && [ $file != $out ]; then
+	cat $file | while read line
+	do
+		if [[ $line =~ $re ]]
+		then
+			month="${BASH_REMATCH[1]}"
+			day="${BASH_REMATCH[2]}"
+			hour="${BASH_REMATCH[3]}"
+			user="${BASH_REMATCH[4]}"
+			ip="${BASH_REMATCH[5]}"
+			echo "$month $day $hour $user $ip" >> $out
+		fi	
 
+		if [[ $line =~ $re2 ]]
+                then
+                        month="${BASH_REMATCH[1]}"
+                        day="${BASH_REMATCH[2]}"
+                        hour="${BASH_REMATCH[3]}"
+                        user="${BASH_REMATCH[4]}"
+                        ip="${BASH_REMATCH[5]}"
+                        echo "$month $day $hour $user $ip" >> $out
+                fi
+
+	done
+    fi
+done

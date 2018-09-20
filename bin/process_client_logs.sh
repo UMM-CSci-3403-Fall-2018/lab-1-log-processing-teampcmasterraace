b587f59eeb5a22489ea3dfd3c58eb0c5ee9a9778
PrinceNwaonicha <38372988+PrinceNwaonicha@users.noreply.github.com>
2018-09-20 00:52:36 -0500

    Final Process_Client_logs
    Tests all pass for Process Client logs. Learned that when going through the files it is better to cd down to the lowest level. Also realized that we needed to use two seperate awks to get the program to work.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,73 +1,13 @@
 #!/bin/bash
 
-: '
-  A few comments about the failing nature of the tests for this function:
+# Author Prince
 
-  Our code was written based on the specifications listed in the README.MD file.
-  Our code was tested in an enviroment based on the specifications listed in the README.MD file.
-  Our code was NOT build to withstand wish-washy and false specifications.
+#setup
+directory=$1
 
-  >>> The specifications are wrong <<<
+#Goes inside the directories log files.
+cd $directory/var/log
 
-  Wrong number one:
-
-    Claim:
-      "This script takes a directory as a command line argument and does all its work in that directory.
-      That directory is assumed to contain the un-tarred and un-gzipped log files for a single client,
-      but we dont know in advance how many files there are or what theyre called."
-
-    Reality:
-      The log files are contained within a nested directory structure: var/log/*,
-      Looping through the supplied directory for files (as the specification suggests) will reveal no files.
-
-  Wrong number two:
-    Claim:
-      "The secure files contain a wide variety of entries documenting numerous different events,
-      but we only care about two particular kinds of lines."
-
-    Reality:
-      You also capture a third line type, which appears in test_sorted.txt as:
-        "Jul 13 02 db2inst1 121.88.249.94"
-      This is incorectly captured from the line:
-        "Jul 13 02:14:02 discovery sshd[4312]: Invalid user db2inst1 from 121.88.249.94"
-'
-
-#Setup
-dir=$1
-re="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* user ([a-z]+) from (.*) port"
-re2="([A-Z][a-z]{2}) ([0-9]{1,2}) ([0-9][0-9]).* for ([a-z]+) from (.*) port"
-cd $dir
-touch failed_login_data.txt
-
-#Iterate over all files & all lines
-for file in var/log/*; do
-	cat $file | while read line
-	do
-
-    #Check for login-type 1
-		if [[ $line =~ $re ]]
-		then
-			month="${BASH_REMATCH[1]}"
-			day="${BASH_REMATCH[2]}"
-			hour="${BASH_REMATCH[3]}"
-			user="${BASH_REMATCH[4]}"
-			ip="${BASH_REMATCH[5]}"
-
-      #append to file
-			echo "$month $day $hour $user $ip" >> failed_login_data.txt
-		fi
-
-    #Check for login-type 2
-		if [[ $line =~ $re2 ]]
-    then
-      month="${BASH_REMATCH[1]}"
-      day="${BASH_REMATCH[2]}"
-      hour="${BASH_REMATCH[3]}"
-      user="${BASH_REMATCH[4]}"
-      ip="${BASH_REMATCH[5]}"
-
-      #append to file
-      echo "$month $day $hour $user $ip" >> failed_login_data.txt
-    fi
-	done
-done
+#Iterate over all files and lines
+cat * | awk '/Failed password for invalid user/ {print $1 $2 substr($3,0,2) $11 $13}' | cat > $directory/failed_login_data.txt
+cat * | awk '/Failed password/ && !/invalid/  {print $1 $2 substr($3,0,2) $9 $11}' | cat >> $directory/failed_login_data.txt

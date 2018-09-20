89bbca2446f5f71d20c4f463519e5093261ef933
SirLich <lrkoehler77@gmail.com>
2018-09-20 11:43:21 -0500

    Added comments + author credit

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,9 +1,18 @@
-dir=$1
+#!/bin/bash
 
+#Author SirLich (Liam Koehle)
+#GitHub: https://github.com/SirLich
+
+#Setup
+dir=$1
 touch $dir/data_out
 touch $dir/failed_login_data.html
+
+#concat files
 cat $dir/country_dist.html $dir/hours_dist.html $dir/username_dist.html >> $dir/data_out
 
+#Wrap
 ./bin/wrap_contents.sh $dir/data_out html_components/summary_plots $dir/failed_login_summary.html
 
+#cleanup
 rm $dir/data_out

7fe7d1a3338abe10366ba11a4706101dec3f77c3
SirLich <lrkoehler77@gmail.com>
2018-09-20 22:12:51 -0500

    Added spaces between columns in failed_login_data.txt
    The tests didn't catch this?

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -9,5 +9,5 @@ directory=$1
 cd $directory/var/log
 
 #Iterate over all files and lines
-cat * | awk '/Failed password for invalid user/ {print $1 $2 substr($3,0,2) $11 $13}' | cat > $directory/failed_login_data.txt
-cat * | awk '/Failed password/ && !/invalid/  {print $1 $2 substr($3,0,2) $9 $11}' | cat >> $directory/failed_login_data.txt
+cat * | awk '/Failed password for invalid user/ {print $1 " " $2 " " substr($3,0,2) " "$11 " "$13}' | cat > $directory/failed_login_data.txt
+cat * | awk '/Failed password/ && !/invalid/  {print $1 " " $2 " " substr($3,0,2) " " $9 " " $11}' | cat >> $directory/failed_login_data.txt

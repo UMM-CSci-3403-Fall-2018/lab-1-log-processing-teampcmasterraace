ad8c5c8ab249eb3a69740d6a6b0a3695a7317a75
SirLich <lrkoehler77@gmail.com>
2018-09-20 22:22:22 -0500

    Added comments for readability

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 #Author: SirLich (Liam Koehler)
+#GitHub: https://github.com/SirLich
 
+#Create temp directory
 TMP_DIR=`mktemp --directory`
-echo $TMP_DIR
+
+#Loop over all args
 for file in "$@"
 do
   name="$(basename $file _secure.tgz)"
-  echo $name
   mkdir $TMP_DIR/$name
   tar -xzf $file -C $TMP_DIR/$name
+
+  #Generated logs
   ./bin/process_client_logs.sh $TMP_DIR/$name
 done
 
+#Process the generated logs
 ./bin/create_username_dist.sh $TMP_DIR
 ./bin/create_hours_dist.sh $TMP_DIR
 ./bin/create_country_dist.sh $TMP_DIR
-
 ./bin/assemble_report.sh $TMP_DIR
 
+#Copy back to working dir
 cp $TMP_DIR/failed_login_summary.html .
 
+#Cleanup
 rm $TMP_DIR -rf
